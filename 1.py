eef5442cde06b8728f5b6357100dc1d1f3bb5262
Roman <mamchiy@yandex.ru>
2021-02-23 22:32:47 +0300

    code optimization

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
 import os
 import PIL.Image
 
+from PyPDF2 import PdfFileMerger
+from pathlib import Path
+
 def img2pdf(fname):
     filename = fname
     name = filename.split('.')[0]
@@ -14,3 +17,20 @@ def img2pdf(fname):
 files = [f for f in os.listdir('./') if f.endswith('.jpg')]
 for fname in files:
     img2pdf(fname)
+
+pdf_merger = PdfFileMerger()
+reports_dir = (
+    Path.cwd()
+    / "output_folder"
+)
+output_folder= list(reports_dir.glob("*.pdf"))
+output_folder.sort()
+
+for path in output_folder:
+    print(path.name)
+
+for path in output_folder:
+    pdf_merger.append(str(path))
+
+with Path("merges.pdf").open(mode="wb") as output_file:
+    pdf_merger.write(output_file)
