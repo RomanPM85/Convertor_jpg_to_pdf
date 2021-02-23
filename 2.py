e8742d19a1a2bd0f76c322d9c5c42a350a06f694
Roman <mamchiy@yandex.ru>
2021-02-23 16:01:57 +0300

    added merger of all files .pdf

--- 2.py
+++ 2.py
@@ -12,4 +12,8 @@ im2pdf_output.sort()
 for path in im2pdf_output:
     print(path.name)
 
+for path in im2pdf_output:
+    pdf_merger.append(str(path))
 
+with Path("merges.pdf").open(mode="wb") as output_file:
+    pdf_merger.write(output_file)
