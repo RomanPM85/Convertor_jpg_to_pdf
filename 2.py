6a6c253c006ee27ea8b46f0e47da9ab69fd872f1
Roman <mamchiy@yandex.ru>
2021-02-23 13:19:17 +0300

    downloaded program covert jpg to pdf

---
+++ 2.py
@@ -0,0 +1,16 @@
+from PyPDF2 import PdfFileMerger
+
+pdfs = []
+t = 1
+for i in range(10):
+    f = str(t) + ".pdf"
+    pdfs.append(f)
+    t = t + 1
+print(pdfs)
+merger = PdfFileMerger()
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("result.pdf")
+merger.close()
